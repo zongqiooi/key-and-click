7d19c48b69c64ae6839268dcc18c99b2096302cc
Zong Qi <huro710@hotmail.com>
2023-05-08 20:35:26 +0800

    Updated token access token for mongoDB

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NTBmMmQyYzU5NTE3ZDAyOWQxMTczZiIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2ODMyMDc4NzcsImV4cCI6MTY4MzQ2NzA3N30.5J1T2loVl9gEKwZ5CPbwHMp5ScgbE2knaLIGJAAvyE8";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+  .currentUser.accessToken;
 
 export const publicRequest = axios.create({ baseURL: BASE_URL });
 
